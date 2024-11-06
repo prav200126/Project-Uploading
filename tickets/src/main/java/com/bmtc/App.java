98125882fd70644422ef43baa694652ac08b80fe
prav200126 <p54165600@gmail.com>
2024-11-06 17:42:23 +0530

    Update App.java

--- tickets/src/main/java/com/bmtc/App.java
+++ tickets/src/main/java/com/bmtc/App.java
@@ -7,5 +7,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Book ur tickets here");
+        for more information contact manager
     }
 }
