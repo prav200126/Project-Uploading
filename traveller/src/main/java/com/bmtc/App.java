3153a4b743a6a1439086402e77f785dddd6b73f7
prav200126 <p54165600@gmail.com>
2024-11-06 17:47:13 +0530

    Update App.java

--- traveller/src/main/java/com/bmtc/App.java
+++ traveller/src/main/java/com/bmtc/App.java
@@ -7,5 +7,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Enter your  destination and pickup point");
+        for travelling u  need  to  pay extra money
     }
 }
