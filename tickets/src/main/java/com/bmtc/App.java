fda8a84080818bc71ab9a657f8695461c170bee4
prav200126 <p54165600@gmail.com>
2024-11-06 17:40:38 +0530

    ticket

---
+++ tickets/src/main/java/com/bmtc/App.java
@@ -0,0 +1,11 @@
+package com.bmtc;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Book ur tickets here");
+    }
+}
