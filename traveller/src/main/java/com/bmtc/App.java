a051b528df0def7adf09034340430e1e80acf417
prav200126 <p54165600@gmail.com>
2024-11-06 17:46:01 +0530

    travel

---
+++ traveller/src/main/java/com/bmtc/App.java
@@ -0,0 +1,11 @@
+package com.bmtc;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Enter your  destination and pickup point");
+    }
+}
